5987a45bf836762fe69390ba75143d19f8fdd0dd
Nar <shakennar90@gmail.com>
2024-10-30 12:37:48 +0500

    ozgerister engizdim

---
+++ homeworks/30.10/script.js
@@ -0,0 +1,10 @@
+var name = "aty"; //ozgeretin man
+let surname = "tegi"; //ozgeredi
+
+surname = "ABC"; //surname ainymalynyn mani ozgeredi
+
+const id = 909090; //turaqty man
+
+id = 84938498; //qate qaitarady
+
+console.log(id); //shyqpaidy

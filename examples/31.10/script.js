99fb2c9f5222bc5b0ee1468bf188e35b563b5f55
Nar <shakennar90@gmail.com>
2024-10-31 12:13:39 +0500

    html js qostym

---
+++ examples/31.10/script.js
@@ -0,0 +1,74 @@
+//let x = 4;
+//let y = 10;
+//let result = x + y; //14
+
+//console.log(result);
+
+
+
+
+
+//var a = 30;
+//var b = 20;
+
+//console.log(a - b); //10
+
+
+
+
+//var x = 45;
+//var y = 5;
+//var result = x * y; //225
+
+//console.log(result);
+
+
+
+
+
+//var c = 20;
+//var d = 2;
+
+//console.log(20 / 2); //10
+
+
+
+
+//var a = 20; 
+/*if (11 % 2 == 0) { // = = 
+    console.log("a sany jup sany");
+}*/
+
+
+
+
+//var num = 100;
+//num ++; //increment
+
+//num --; //decrement
+
+//console.log(num);
+
+
+
+
+
+//var a = 100;
+//a + 5; //qosyp tagaiyndau a = a + 5;
+
+//console.log(a);
+
+
+
+
+//var a = 100;
+//a -= 5; // a = a - 5;
+//a /= 5; // a = a / 5;
+//a *= 5; // a = a * 5;
+//a %= 3; // a = a % 3; a = 1;
+
+//console.log(a);
+
+
+
+
---
+++ examples/31.10/task.js
@@ -0,0 +1,12 @@
+
+
+var a = 200;
+var b = 158;
+var c = 2;
+var z = 15;
+
+
+
+let total = ((a * b) / c) / (z - 3);
+
+console.log(total % 2)

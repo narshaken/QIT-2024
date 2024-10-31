371c3c7e82c9ac40d936681e9db3c2c945bd4d14
Nar <shakennar90@gmail.com>
2024-10-31 12:46:09 +0500

    task js qostym

--- examples/31.10/task.js
+++ examples/31.10/task.js
@@ -1,12 +1,37 @@
 
 
-var a = 200;
-var b = 158;
-var c = 2;
-var z = 15;
+//var a = 101;
+//var b = 67;
+//var c = 55;
+//let total = (((a / b) * 45) + 56 / (c - b - a) / 10);
+//console.log(total)
+
+//var p = (a + b) * 2;
+//var f = mg;
+
+
+//var x = 105; //tagaiyndau
+//var y = 40;
+//salystyru operatory
+/*if ( x > y ) {
+    console.log(x, "ulkenirek");
+} else {
+    console.log(y, "ulkenirek")
+}*/
 
 
 
-let total = ((a * b) / c) / (z - 3);
+var x = 100;
+// = = ten ekenin tekseru
+if ( x === 100) {
+    console.log("keremet, jaraisyn");
+}
+  else if ( x > 80) {
+    console.log("jaman emes");
+} else if ( x > 50) {
+    console.log("tagy oqu kerek")
+}
 
-console.log(total % 2)
+
+
+

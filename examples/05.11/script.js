bf68aab2d4c6cc326875bab15470a04cb87ad123
Nar <shakennar90@gmail.com>
2024-11-05 13:49:55 +0500

    ui jumysy IT

---
+++ examples/05.11/script.js
@@ -0,0 +1,123 @@
+//alert("hello world " + "hello" + "\n" + "2024");
+
+//prompt("how are you");
+//let name = prompt("enter your name");
+
+//alert("hello, " + name);
+
+
+
+
+//let name = prompt("enter your name");
+//let email = prompt("enter your email");
+//let age = prompt("enter your age");
+//alert("hello, " + name + "\n" + "you are " + age + "\n" + "your email " + email);
+
+
+
+
+// var num = prompt("Enter any number: "); //san engizildi
+
+//if (num % 2 == 0) { //jup ekenin tekseremiz
+    //alert("number " + num + " is even");
+//} else {
+    //alert("number " + num + " is odd");
+//}
+
+
+
+
+//var birthYear = prompt("enter year of birth: ");
+
+//alert(2024 - birthYear); //NaN not a number
+
+
+
+
+
+//var birthYear = Number(prompt("enter year of birth"));
+//var age = "your age " + (2024 - birthYear);
+
+/*if (isNaN(birthYear)) {
+    alert("enter number");
+} else {
+    alert(age);
+}*/
+
+
+
+
+//var password = prompt("Enter a password: ");
+//var correctPassword = "shakennar";
+
+//if (password == correctPassword) {
+    //alert("Welcome")
+//} else {
+    //alert("Password is wrong")
+//}
+
+
+
+
+
+
+
+/*var userAns1 = prompt("Men qai jyly duniege keldim?").toString();
+var userAns2 = prompt("Kazakhstan qai jyly tauelsizdik aldy?").toString();
+var userAns3 = prompt("Men qai jyly duniege keldim?").toString();
+var userAns4 = prompt("Kazakhstan qai jyly tauelsizdik aldy?").toString();
+var userAns5 = prompt("Kazakhstan qai jyly tauelsizdik aldy?").toString();
+var score = 0;
+
+if (userAns1 == "2005" || userAns1 == "2005") {
+    score++;
+} 
+if (userAns2 == "1991" || userAns2 == "1991") {
+    score++;
+} 
+if (userAns3 == "2005" || userAns3 == "2005") {
+    score++;
+} 
+if (userAns4 == "1991" || userAns4 == "1991") {
+    score++;
+} 
+if (userAns5 == "1991" || userAns5 == "1991") {
+    score++;
+} 
+
+alert("siz " + score + " upai aldynyz!");*/
+
+
+
+
+
+
+
+/*var score = 0;
+
+var userAns1 = prompt("men qai jyly tuyldym?").toString();
+if (userAns1 == "2005" || userAns1 == "2005") {
+    score++;
+    alert("+1");
+} else {
+    alert("qate!");
+}
+
+var userAns1 = prompt("men qai jyly tuyldym?").toString();
+if (userAns1 == "2005" || userAns1 == "2005") {
+    score++;
+    alert("+1");
+} else {
+    alert("qate!");
+}*/
+
+
+
+
+// 239 F
+var temperature = prompt("Enter temperature in Farenheit: ");
+var celsius = (temperature / 33.8);
+
+alert(Math.round(celsius));
+
+

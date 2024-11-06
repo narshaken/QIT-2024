aa015badfddb26785049c6c73e93251ee216465f
Nar <shakennar90@gmail.com>
2024-11-06 09:28:48 +0500

    ui tapsyrmasyyy 05.11

---
+++ homeworks/05.11/script.js
@@ -0,0 +1,24 @@
+//tapsyrma 1
+var age = prompt("Enter your name");
+if (age > 18) {
+    alert("Siz ereseksiz!");
+} else {
+    alert("Siz kamelettik jasqa tolmagansyz!");
+}
+
+
+
+//tapsyrma 2
+var weather = prompt("Weather");
+if (weather > 20 && weather < 20) {
+    alert("Shorty, futbolka kigeniniz durys");
+} else {
+    alert("Qalyn kiim kiiniz");
+}
+
+
+
+
+
+
+

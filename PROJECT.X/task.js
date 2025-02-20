431a9cac60841fe155c86ac4d30bb38ff670c2e8
Nar <shakennar90@gmail.com>
2025-02-20 13:10:25 +0500

    project lala

--- PROJECT.X/task.js
+++ PROJECT.X/task.js
@@ -4,7 +4,8 @@ let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
 let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar')
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn')
 
 window.onscroll = () => {
     searchBtn.classList.remove('fa-times');
@@ -31,3 +32,12 @@ formBtn.addEventListener('click', () => {
 formClose.addEventListener('click', () => {
     loginForm.classList.remove('active');
 });
+
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    })
+})
